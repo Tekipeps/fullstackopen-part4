a133831995e5539b338f6d0f41c1c07ca926df7a
Tekena <tekipeps@localhost.localdomain>
2020-07-03 10:50:09 +0100

    first commit

---
+++ utils/list_helper.js
@@ -0,0 +1,22 @@
+const dummy = (blogs) => {
+  return 1;
+};
+
+const totalLikes = (blogs) => {
+  const reducer = (sum, item) => {
+    return sum + item;
+  };
+  const total = blogs.map((blog) => blog.likes).reduce(reducer, 0);
+  return blogs.length === 0 ? 0 : total;
+};
+
+const favoriteBlog = (blogs) => {
+    const likes = blogs.map(blog => blog.likes)
+    const {_id, url, __v, ...favorite} = blogs[likes.indexOf(Math.max(...likes))]
+    return favorite
+}
+module.exports = {
+  dummy,
+  totalLikes,
+  favoriteBlog
+};
